efb7a6b4c44c6f23736526d1fa19e6b38df9dc0f
santi0808 <santiagogabrielaldana@gmail.com>
2024-10-17 20:56:39 +0000

    sdfghjklñ

---
+++ santiago.py
@@ -0,0 +1,11 @@
+nombre='santiago
+numero=3
+booleano=True
+booleano=False
+print(numero)
+print(3)
+EdadDeSantiago=16
+EdadParaVotar=16
+puedevotar=EdadParaVotar<=EdadDeSantiago
+if puedevotar:
+    print('puede votar')
---
+++ while.py
@@ -0,0 +1,8 @@
+condicion=True
+contador=0
+while condicion:
+     contador=contador+1
+      condicion=contador<10
+      print(contador)
+
+    
